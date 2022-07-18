6622adae91b719efa1ef174380fcaf22b72a0ed4
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-18 21:05:25 +0300

    task1 вщту

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,4 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-// и возвращает значение этого элемента или же указание, что такого элемента нет.
+﻿// 1. Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
 
 // Например, задан массив:
 
@@ -9,61 +8,64 @@
 
 // 8 4 2 4
 
-// 17 -> такого числа в массиве нет
+// В итоге получается вот такой массив:
 
-namespace Seminar8
+// 8 4 2 4
+
+// 5 9 2 3
+
+// 1 4 7 2
+
+
+
+class Program
 {
-    class Task1
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int n = new Random().Next(3, 7);
+        int m = new Random().Next(3, 7);
+        int[,] array = new int[m, n];
+        FillArray(array);
+        Console.WriteLine($"Изначальный массив:  ");
+        PrintArray(array);
+        Console.WriteLine("Перевернутый массив: ");
+        ReplacLine(array);
+        PrintArray(array);
+    }
+    static void FillArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            int m = new Random().Next(1, 11);
-            int n = new Random().Next(1, 11);
-            int[,] array = new int[m, n];
-            FillArray(array);
-            Console.WriteLine("Введите позицию элемента в двумерном массиве.");
-            Console.WriteLine("номер строки (от 1 до 10):");
-            int str = CheckInputNumbers();
-            Console.WriteLine("номер столбца (от 1 до 10):");
-            int column = CheckInputNumbers();
-            Console.WriteLine($"Случайно сгенерированный двумерный массив с количеством строк {m} и коллличеством столбцов {n}");
-            PrintArray(array);
-            if (str > array.GetLength(0) || column > array.GetLength(1)) Console.WriteLine("Такого элемента нет!");
-            else Console.WriteLine($"Такаой элемент есть и равен: {array[str - 1, column - 1]}");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = new Random().Next(0, 10);
+            }
+        }
+    }
+    static void ReplacLine(int[,] array)
+    {
+        int lineEnd = array.GetLength(0) - 1;
+        for (int i = 0; i < 1; i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                int number = array[i, j];
+                array[i, j] = array[lineEnd, j];
+                array[lineEnd, j] = number;
+            }
 
         }
 
-        static void FillArray(int[,] array)
+    }
+    static void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int j = 0; j < array.GetLength(1); j++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = new Random().Next(1, 10);
-                }
+                Console.Write(array[i, j] + " ");
             }
-        }
-
-        static void PrintArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
-        static int CheckInputNumbers()
-        {
-            int number;
-            while (!int.TryParse(Console.ReadLine(), out number) || number > 10 || number < 1)
-            {
-                Console.WriteLine("Ошибка ввода! Введите целое число от 1 до 10 ");
-            }
-            return number;
-
+            Console.WriteLine();
         }
     }
 }
